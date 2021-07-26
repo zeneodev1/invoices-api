5efb341af3d082f549f2bcc74dee780202ec4c5f
zeneodev1 <zeneodev1@gmail.com>
2021-07-26 13:36:34 +0100

    invoices

--- src/main/java/com/zeneo/invoice/dao/Invoice.java
+++ src/main/java/com/zeneo/invoice/dao/Invoice.java
@@ -4,6 +4,8 @@ import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.List;
+
 @Data
 @Document
 public class Invoice {
@@ -15,5 +17,5 @@ public class Invoice {
     private InvoiceTerms terms;
     private String description;
     private InvoiceStatus status;
-
+    private List<InvoiceItem> invoiceItems;
 }
